34f65ac052c9b2099b9445b5719c9592835e3beb
Kyle Bradshaw <kyle.bradshaw@gmail.com>
2020-07-08 00:36:37 -0400

    randomize some numbers

--- src/app/components/cardiac-output/cardiac-output.component.ts
+++ src/app/components/cardiac-output/cardiac-output.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getRandomArbitrary } from 'src/app/app.utils';
 
 @Component({
   selector: 'app-cardiac-output',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cardiac-output.component.sass']
 })
 export class CardiacOutputComponent implements OnInit {
+  output = 4.5;
+  power = 0.7;
 
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.output = getRandomArbitrary(1, 2) % 2 === 0 ? this.output + 0.1 : this.output - 0.1;
+      this.output = +this.output.toFixed(1);
+    }, getRandomArbitrary(5000, 20000));
+    setInterval(() => {
+      this.power = getRandomArbitrary(1, 2) % 2 === 0 ? this.power + 0.1 : this.power - 0.1;
+      this.power = +this.power.toFixed(1);
+    }, getRandomArbitrary(5000, 20000));
   }
 
 }
--- src/app/components/impella-flow/impella-flow.component.ts
+++ src/app/components/impella-flow/impella-flow.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getRandomArbitrary } from 'src/app/app.utils';
 
 @Component({
   selector: 'app-impella-flow',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./impella-flow.component.scss']
 })
 export class ImpellaFlowComponent implements OnInit {
-
+  numerator = 5.8;
+  denominator = 4.5;
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.numerator = getRandomArbitrary(1, 2) % 2 === 0 ? this.numerator + 0.1 : this.numerator - 0.1;
+      this.numerator = +this.numerator.toFixed(1);
+    }, getRandomArbitrary(5000, 20000));
+    setInterval(() => {
+      this.denominator = getRandomArbitrary(1, 2) % 2 === 0 ? this.denominator + 0.1 : this.denominator - 0.1;
+      this.denominator = +this.denominator.toFixed(1);
+    }, getRandomArbitrary(5000, 20000));
   }
-
 }
